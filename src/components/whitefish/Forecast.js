851fdf5c6388cd16c4d6e419155398da8c55eecc
Pouria <charliswager@gmail.com>
2019-04-05 13:13:57 +0430

    moment lib added to format unix

--- src/components/whitefish/Forecast.js
+++ src/components/whitefish/Forecast.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import { View, Text, Image } from "react-native";
 import { connect } from "react-redux";
+import { unix } from "moment";
 
 import renderIcon from "../renderIcon";
 
 class Forecast extends Component {
+  unixTimeConvetor(number) {
+    var day = unix(number);
+    return day._d.toString();
+  }
   render() {
     const { icon } = this.props.state.data;
     return (
       <View>
         <Text> Forecast </Text>
-        <Text>ds</Text>
+        <Text>{this.unixTimeConvetor(1554453000)}</Text>
         <Image
           style={{ height: 50, width: 50 }}
           source={require(`../../icons/${this.props.renderIcon}`)}
