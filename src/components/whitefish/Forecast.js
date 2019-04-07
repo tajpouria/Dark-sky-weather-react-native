20b6598d4dc68309ca7e9cc548f1d94670c0a3ea
Pouria <charliswager@gmail.com>
2019-04-08 00:07:41 +0430

    in order toe have row list chanegd from flast list to a methode

--- src/components/whitefish/Forecast.js
+++ src/components/whitefish/Forecast.js
@@ -4,12 +4,25 @@ import { connect } from "react-redux";
 import HourTile from "./HourTile";
 
 class Forecast extends Component {
+  renderHours() {
+    const array = [0, 1, 2, 3, 4, 5];
+    return array.map(item => {
+      return (
+        <HourTile
+          icon={this.props.state.data.hourly[item].icon}
+          temprature={this.props.state.data.hourly[item].temperature}
+          date={this.props.state.data.hourly[item].time}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <View>
         <Text> Forecast </Text>
-        <FlatList
-          data={[0, 1, 2, 4, 6, 8]}
+        {/* <FlatList
+          data={[0, 1, 2, 3, 4, 5]}
           renderItem={({ item }) => (
             <HourTile
               icon={this.props.state.data.hourly[item].icon}
@@ -20,7 +33,8 @@ class Forecast extends Component {
           keyExtractor={i => {
             return i.toString();
           }}
-        />
+        /> */}
+        <View style={{ flexDirection: "row" }}>{this.renderHours()}</View>
       </View>
     );
   }
